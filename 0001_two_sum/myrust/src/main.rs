fc9b213c76b9496953100673117603bc7d2751e2
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-07-22 21:20:23 +0100

    working on rust first thing

--- 0001_two_sum/myrust/src/main.rs
+++ 0001_two_sum/myrust/src/main.rs
@@ -1,3 +1,24 @@
+struct Solution;
+
+impl Solution {
+    fn two_sum_double_loop(x: &[i32]) -> bool {
+        println!("Hello, world!");
+        true
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    Solution::two_sum_double_loop(&[1,2,3]);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::Solution;
+
+    #[test]
+    fn test_sum_double_loop() {
+        assert_eq!(Solution::two_sum_double_loop([1,2,3]), true);
+    }
 }
