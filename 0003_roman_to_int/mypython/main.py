2be1607bcaaf0b1d925356f4fe526fcf04eca80e
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-07-27 19:53:26 +0100

    Setting up testing and the Converter Class

--- 0003_roman_to_int/mypython/main.py
+++ 0003_roman_to_int/mypython/main.py
@@ -2,5 +2,24 @@
 Given Roman numerals this library can convert to them into an integer
 """
 
+import unittest as ut
+
+class Converter:
+    help = "Class to convert between Roman and int"
+
+    @staticmethod
+    def roman_to_int(roman: str):
+        return 0
+    
+class TestConverter(ut.TestCase):
+    def setUp(self):
+        super()
+    
+    def test_roman_to_int(self):
+        self.assertEqual(Converter.roman_to_int('test'), 0)
+
+
 if __name__ == "__main__":
     print('Hello, world!')
+
+    ut.main()
