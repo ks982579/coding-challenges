454123abb38856fd969002370cba84cf17b58128
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-08-09 06:52:48 +0100

    Working on mypython version to maths CLI game

--- 0006_cli_maths_game/mypython/components/components.py
+++ 0006_cli_maths_game/mypython/components/components.py
@@ -1,7 +1,16 @@
 """
 Helper classes and methods for the game
 """
+from random import randint
 
 class MathsGame():
     def __init__(self):
         print("Hello, world")
+        self._numbers = [x+1 for x in range(10)]
+    
+    def _select_numbers(self) -> None:
+        x = randint(0, len(self._numbers))
+        y = randint(0, len(self._numbers))
+
+    def _write(self, a: int, b: int, op: str) -> None:
+        print(f"{a} {op} {b}")
---
+++ 0006_cli_maths_game/mypython/main.py
@@ -0,0 +1,6 @@
+"""
+To play a maths adding and multiplication game
+"""
+
+if __name__ == "__main__":
+    print('5 + 6')
