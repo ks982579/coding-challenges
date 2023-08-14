f975b1d05c08bbfdaf49233d45925d9ff30ce9e7
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-08-14 21:31:14 +0100

    Setting up python solution

---
+++ 0008_nth_largest_elm/mypython/main.py
@@ -0,0 +1,15 @@
+"""
+To find nth largest integer in array w/out sorting it!
+"""
+
+import array
+import unittest
+
+
+class TestFindNthLargestElement(unittest.TestCase):
+    def test_finding_nth_largest(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    print('Hello, world!')
+    unittest.main()
