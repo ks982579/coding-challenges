33ded2a2f1d101abb697c9ea98a5369163c90a08
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-10-28 21:30:38 +0100

    adding attack types

--- 0011_text_game/rustverse/src/main.rs
+++ 0011_text_game/rustverse/src/main.rs
@@ -1,9 +1,30 @@
 pub mod beings {
+    #[derive(Default, Debug, Clong)]
+    pub enum AttackTypes {
+        physical,
+        fire,
+        electric,
+        ice,
+    }
+
+    pub struct BasicAttack {
+        pub attack_types: Vec<AttackTypes>,
+        pub damage: u32,
+        pub effects: Optional<str>,
+    }
+
     pub struct Being {
         pub name: String,
         pub health: u32,
+        pub strength: u32,
     }
 }
+
+/**
+ * url: https://stackoverflow.com/questions/19650265/is-there-a-faster-shorter-way-to-initialize-variables-in-a-rust-struct
+ * You can implement Default trait for struct
+ * Then use spread syntax 
+ */
 
 use crate::beings::Being;
 
@@ -12,6 +33,7 @@ fn main() {
     let batman: Being = Being {
         name: String::from("Batman"),
         health: 100,
+        strength: 10,
     };
 }
 
