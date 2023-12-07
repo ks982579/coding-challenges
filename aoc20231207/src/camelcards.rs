806720ac605283dcebc951b0a0728d8ae8c29933
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-12-07 20:38:12 +0000

    added checks for 5 of kind, 4 of kind and full house

--- aoc20231207/src/camelcards.rs
+++ aoc20231207/src/camelcards.rs
@@ -5,6 +5,33 @@ pub struct Hand<'l> {
     pub rank: u64,
 }
 
+fn card_counter(cards: &str, card_counts: &mut Vec<(char, u64)>) {
+    /* Function to count cards in hands and put into vector */
+    for card in cards.chars() {
+        if card_counts.len() < 1 {
+            card_counts.push((card, 1));
+        } else {
+            let mut added: bool = false;
+            for ctup in card_counts.clone().iter_mut().enumerate() {
+                // (indes, (card, count))
+                if card == ctup.1.0 {
+                    card_counts[ctup.0].1 += 1;
+                    added = true;
+                    break;
+                }
+            }
+            if !added {
+                card_counts.push((card, 1));
+            }
+        }
+    }
+}
+
+/*
+ * Word of Caution: The checks will not be mutually exclusive because they
+ * are meant to be executed in order. That means the is_one_pair() will return
+ * true for is_two_pair() and so on...
+ */
 impl<'l> Hand<'l> {
     pub fn from_str(data: &'l str) -> Hand<'l> {
         let data_split: Vec<&str> = data.trim().split_whitespace().collect::<Vec<&str>>();
@@ -15,6 +42,50 @@ impl<'l> Hand<'l> {
             bid: current_bid,
             rank: 0,
         }
+    }
+
+    pub fn is_five_of_kind(&self) -> bool {
+        let mut mem: Option<char> = None;
+
+        for card in self.cards.chars() {
+            match mem {
+                None => mem = Some(card),
+                Some(prev_card) => {
+                    if prev_card != card {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+    pub fn is_four_of_kind(&self) -> bool {
+        // (card, count)
+        let mut cc: Vec<(char, u64)> = Vec::with_capacity(5);
+        
+        card_counter(self.cards, &mut cc);
+        cc.sort_by(|a,b| b.1.cmp(&a.1));
+        if cc.len() == 2 {
+            if cc[0].1 == 4 && cc[1].1 == 1 {
+                return true;
+            }
+        }
+        false
+    }
+    
+    pub fn is_full_house(&self) -> bool {
+        // (card, count)
+        let mut cc: Vec<(char, u64)> = Vec::with_capacity(5);
+        
+        card_counter(self.cards, &mut cc);
+        cc.sort_by(|a,b| b.1.cmp(&a.1));
+
+        if cc.len() == 2 {
+            if cc[0].1 == 3 && cc[1].1 == 2 {
+                return true;
+            }
+        }
+        false
     }
 }
 
@@ -37,5 +108,29 @@ mod tests {
         };
         let actual: Hand = Hand::from_str(data);
         assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_hand_is_5_of_kind() {
+        let data: &str = "TTTTT 0";
+        let hand = Hand::from_str(data);
+        assert!(hand.is_five_of_kind());
+    }
+    #[test]
+    fn test_hand_is_4_of_kind() {
+        let data: &str = "KTTTT 0";
+        let hand = Hand::from_str(data);
+        assert!(hand.is_four_of_kind());
+        let data: &str = "TTTTQ 0";
+        let hand = Hand::from_str(data);
+        assert!(hand.is_four_of_kind());
+    }
+    #[test]
+    fn test_hand_is_full_house() {
+        let data: &str = "KTTTK 0";
+        let hand = Hand::from_str(data);
+        assert!(hand.is_full_house());
+        let data: &str = "TTQTQ 0";
+        let hand = Hand::from_str(data);
+        assert!(hand.is_full_house());
     }
 }
--- aoc20231207/src/main.rs
+++ aoc20231207/src/main.rs
@@ -5,11 +5,18 @@ use std::fs::read_to_string;
 mod camelcards;
 use camelcards::*;
 
+/*
+sting splits to hand and bid
+
+*/
 fn main() {
     let mut puzzle_path: PathBuf = current_dir().unwrap();
     puzzle_path.push("puzzle.txt");
     let puzzle: String = read_to_string(puzzle_path).unwrap();
+    let mut all_hands: Vec<Hand> = Vec::new();
     for hand in puzzle.lines() {
-        todo!();
+        all_hands.push(
+            Hand::from_str(hand)
+        )
     }
 }
