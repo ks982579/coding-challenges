7b76fa23dcf0310cb5dc2ae45eada712deacaad0
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2024-06-14 22:25:50 +0100

    working on twosum logic for C++

---
+++ 0001_two_sum/mycpp/README.md
@@ -0,0 +1,8 @@
+# C++ Notes
+
+I am also using [Conan](https://docs.conan.io/2/tutorial/creating_packages/create_your_first_package.html).
+This package was made with the following command:
+
+```bash
+conan new cmake_lib -d name=twosum -d version=1.0
+```
--- 0001_two_sum/mycpp/src/twosum.cpp
+++ 0001_two_sum/mycpp/src/twosum.cpp
@@ -1,30 +1,50 @@
+#include <algorithm> // for sorting
 #include <iostream>
+#include <tuple>
 #include <vector>
 
 /* Have fun with this
  * find the largest number in list that is smaller than output
- * >> could be weird binary search
+ * >> could be weird binary search, requires sorting
  * In remaining list, search for compatible number.
  * */
 
 class Computer {
-    private:
-        std::vector<int> numbers;
-    public:
-        Computer(const std::vector<int> &data) {
-            numbers = data; // hopefully performs copy
-        }
+private:
+  std::vector<int> numbers;
+  int calc_len() { return sizeof(numbers) / sizeof(int); }
+
+public:
+  int length;
+  Computer(const std::vector<int> &data) {
+    numbers = data; // hopefully performs copy
+    std::sort(numbers.begin(), numbers.end());
+    length = calc_len();
+  }
+
+  // index_closest_small_value()
+  unsigned int index_closest_small_value() { return 42; }
+
+  // two_sum()
+  std::tuple<int, int> two_sum(int a, int b) {
+    size_t ind = index_closest_small_value();
+    return std::make_tuple(0, 0);
+  }
 };
 
+void tests() {
+  std::vector<int> list = {10, 13, 22, 55, 42};
+  Computer cmp = Computer(list);
+}
+
 // return indices of two numbers that sum to target.
-int main () {
-    std::vector<int> list = {
-        2,3,4,7,11,13,17,19,13,29,
-        31,37,41,43,47,53,59,61,67,71,
-        73,79,83,89,97,101,103,107,109,113,
-        127,131,137,139,149,151,157,163,167,173
-    };
-    Computer comp = Computer(list);
-    std::cout << "Hello, world";
-    return 0;
+int main() {
+  std::vector<int> list = {2,   3,   4,   7,   11,  13,  17,  19,  13,  29,
+                           31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
+                           73,  79,  83,  89,  97,  101, 103, 107, 109, 113,
+                           127, 131, 137, 139, 149, 151, 157, 163, 167, 173};
+  // Creates copy... Chaning ownership may optimize.
+  Computer comp = Computer(list);
+  std::cout << "Hello, world";
+  return 0;
 }
