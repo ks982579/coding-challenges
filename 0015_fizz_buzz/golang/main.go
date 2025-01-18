f4681ad00a60e1dfb0a634997a779c6b053aab2b
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-18 11:22:55 +0000

    Adding line to print all numbers

--- 0015_fizz_buzz/golang/main.go
+++ 0015_fizz_buzz/golang/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, world!");
+  fmt.Println("FizzBuzz challenge in Go!");
+  // first print all numbers between one and 100
+  for i := 1; i <= 100; i++ {
+    fmt.Println(i);
+  }
 }
