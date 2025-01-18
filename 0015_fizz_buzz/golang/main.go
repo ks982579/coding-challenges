7911a5f13402583040c25d1d6a3a3820f76d116b
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-18 11:28:54 +0000

    Updating FizzBuzz logic in Go

--- 0015_fizz_buzz/golang/main.go
+++ 0015_fizz_buzz/golang/main.go
@@ -6,8 +6,16 @@ func main() {
   fmt.Println("FizzBuzz challenge in Go!");
   // first print all numbers between one and 100
   for i := 1; i <= 100; i++ {
+    var message string = "";
     if i % 3 == 0 {
-      fmt.Println("Fizz");
+      message += "Fizz";
+    }
+    if i % 5 == 0 {
+      message += "Buzz";
+    }
+
+    if message != "" {
+      fmt.Println(message);
     } else {
       fmt.Println(i);
     }
