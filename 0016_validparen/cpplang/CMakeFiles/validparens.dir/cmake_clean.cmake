79de3d6f9ff311048a2e8490400cb3ca20c57364
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-19 12:49:13 +0000

    Setting up the project with a CMake file.

---
+++ 0016_validparen/cpplang/CMakeFiles/validparens.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/validparens.dir/src/main.cpp.o"
+  "CMakeFiles/validparens.dir/src/main.cpp.o.d"
+  "target/validparens"
+  "target/validparens.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/validparens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 0016_validparen/cpplang/CMakeFiles/validparens.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for validparens.
+# This may be replaced when dependencies are built.
