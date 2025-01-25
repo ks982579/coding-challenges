c7837ee4ae9adb23e4580e48e0baeb025445d2c7
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-25 09:15:07 +0000

    Added another test case, seems OK

--- 0020_flood_fill/cpplang/tests/main.cpp
+++ 0020_flood_fill/cpplang/tests/main.cpp
@@ -8,12 +8,19 @@ int main() {
   // using Image = std::vector<std::vector<int>>;
   TestFramework tf;
 
-  tf.addTest("Testing Quacky Test Framework", [&tf]() -> void {
+  tf.addTest("Standard first test", [&tf]() -> void {
     Image image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-    // std::vector<int> nums({-1, 0, 3, 5, 9, 12});
     std::cout << "In Test" << std::endl;
     Image actual = Solution::floodFill(image, 1, 1, 2);
     Image expected = {{2, 2, 2}, {2, 2, 0}, {2, 0, 1}};
+    tf.assertEqual(expected, actual);
+  });
+
+  tf.addTest("No Fill", [&tf]() -> void {
+    Image image = {{1, 2, 3, 1}, {4, 3, 3, 2}, {1, 1, 0, 5}, {1, 1, 1, 9}};
+    std::cout << "In Test" << std::endl;
+    Image actual = Solution::floodFill(image, 2, 2, 8);
+    Image expected = {{1, 2, 3, 1}, {4, 3, 3, 2}, {1, 1, 8, 5}, {1, 1, 1, 9}};
     tf.assertEqual(expected, actual);
   });
 
