12c570099cd3ab7e2cab594e063a30e9a1061039
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-27 15:16:58 +0000

    Adding more tests to find a bug

--- 0021_lowest_common_ancestor_bst/cpplang/tests/main.cpp
+++ 0021_lowest_common_ancestor_bst/cpplang/tests/main.cpp
@@ -35,6 +35,63 @@ int main() {
     tf.assertEqual(expected, actual);
   });
 
+  tf.addTest("Deeper nested test", [&tf]() -> void {
+    TreeNode *tr = new TreeNode(6);
+    tr->left = new TreeNode(2);
+    tr->left->left = new TreeNode(0);
+    tr->left->right = new TreeNode(4);
+    tr->left->right->left = new TreeNode(3);
+    tr->left->right = new TreeNode(5);
+    tr->right = new TreeNode(8);
+    tr->right->left = new TreeNode(7);
+    tr->right->right = new TreeNode(9);
+
+    TreeNode *p = new TreeNode(0);
+    TreeNode *q = new TreeNode(5);
+
+    TreeNode *actual = Solution::lowestCommonAncestor(tr, p, q);
+    TreeNode *expected = new TreeNode(2);
+    tf.assertEqual(expected, actual);
+  });
+
+  tf.addTest("Deepest Nested Test", [&tf]() -> void {
+    TreeNode *tr = new TreeNode(6);
+    tr->left = new TreeNode(2);
+    tr->left->left = new TreeNode(0);
+    tr->left->right = new TreeNode(4);
+    tr->left->right->left = new TreeNode(3);
+    tr->left->right = new TreeNode(5);
+    tr->right = new TreeNode(8);
+    tr->right->left = new TreeNode(7);
+    tr->right->right = new TreeNode(9);
+
+    TreeNode *p = new TreeNode(3);
+    TreeNode *q = new TreeNode(9);
+
+    TreeNode *actual = Solution::lowestCommonAncestor(tr, p, q);
+    TreeNode *expected = new TreeNode(6);
+    tf.assertEqual(expected, actual);
+  });
+
+  tf.addTest("Same node is ancestor", [&tf]() -> void {
+    TreeNode *tr = new TreeNode(6);
+    tr->left = new TreeNode(2);
+    tr->left->left = new TreeNode(0);
+    tr->left->right = new TreeNode(4);
+    tr->left->right->left = new TreeNode(3);
+    tr->left->right = new TreeNode(5);
+    tr->right = new TreeNode(8);
+    tr->right->left = new TreeNode(7);
+    tr->right->right = new TreeNode(9);
+
+    TreeNode *p = new TreeNode(2);
+    TreeNode *q = new TreeNode(5);
+
+    TreeNode *actual = Solution::lowestCommonAncestor(tr, p, q);
+    TreeNode *expected = new TreeNode(2);
+    tf.assertEqual(expected, actual);
+  });
+
   tf.run();
 
   return 0;
