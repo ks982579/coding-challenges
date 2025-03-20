d9243313fe7390d01f7c5796fb5fc56bc75ff72c
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-03-20 20:58:14 +0000

    The C# solution for Two Sum is alright.

--- 0001_two_sum/mycsharp/Program.cs
+++ 0001_two_sum/mycsharp/Program.cs
@@ -3,22 +3,26 @@
 class EntryPoint {
   static void Main() {
     // Printing 
-    Console.WriteLine("Hello, World 2.0");
+    Console.WriteLine("Running Program...");
     // Array
-    int[] info = { 5, 7, 10, 13, 15 };
-    int target = 24;
-    bool flag = false;
+    TwoSum( new int[] { 5, 7, 10, 13, 14 }, 24);
+    TwoSum( new int[] { 5, 7, 10, 13, 12 }, 4);
+    TwoSum( new int[] { 5, 7, 10, 13, 15 }, 13 + 15);
+    Console.WriteLine("Program Complete");
+  }
 
+  static void TwoSum(int[] nums, int target) {
+    bool flag = false;
     // Doing double loop for now
     // Don't need to check last, it'll be checked.
-    for ( int i = 0; i < info.Length - 1; i++ ) {
+    for ( int i = 0; i < nums.Length - 1; i++ ) {
       if ( flag ) {
         break;
       }
-      Console.WriteLine(info[i]);
-      for ( int j = i + 1; j < info.Length; j++) {
-        Console.WriteLine("  " + info[j]);
-        if( info[i] + info[j] == target ) {
+      // Console.WriteLine(nums[i]);
+      for ( int j = i + 1; j < nums.Length; j++) {
+        // Console.WriteLine("  " + nums[j]);
+        if( nums[i] + nums[j] == target ) {
           flag = true;
           Console.WriteLine("Index Values: " + i + ", " + j);
           break;
