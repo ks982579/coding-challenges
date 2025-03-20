78be2e0291d6d2b2071d4b9ffaaf7434cd132e58
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-03-20 20:31:54 +0000

    Refactoring entrypoint to be more explicit.

--- 0001_two_sum/mycsharp/Program.cs
+++ 0001_two_sum/mycsharp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class EntryPoint {
+  static void Main() {
+    // Printing 
+    Console.WriteLine("Hello, World 2.0");
+  }
+}
